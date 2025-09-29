8a332099a89289dd86882e43e7fbd9ce7df1e32f
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-09-29 15:59:58 -0300

    update: setting modality model as implementing GridItemData

--- lib/models/modality.dart
+++ lib/models/modality.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:olimpo/models/basic_model.dart';
 
-class Modality {
+class Modality implements GridItemData{
   const Modality({
     required this.id,
     required this.title,
@@ -9,10 +10,14 @@ class Modality {
     required this.rules,
     this.color = Colors.orange});
 
+  @override
   final String id;
+  @override
   final String title;
+  @override
   final String imageUrl;
   final String history;
   final String rules;
+  @override
   final Color color;
 }
