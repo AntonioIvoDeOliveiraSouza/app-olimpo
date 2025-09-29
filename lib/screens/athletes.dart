cff615c7a5ca5325a636570e8d4f1f749b1ee530
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-09-29 15:55:47 -0300

    feat: creating athlete grid screen

---
+++ lib/screens/athletes.dart
@@ -0,0 +1,33 @@
+import 'package:olimpo/widgets/modality_grid_item.dart';
+import 'package:flutter/material.dart';
+import 'package:olimpo/data/olympic_data.dart';
+import 'package:olimpo/widgets/nav_bar.dart';
+
+class AthletesScreen extends StatelessWidget {
+  const AthletesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Atletas"),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue,
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(16),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 4/2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20
+        ),
+        children: [
+          for (final athlete in listAthlete)
+            ModalityGridItem(athlete: athlete)
+        ],
+      ),
+      bottomNavigationBar: NavBar(parentContext: context),
+    );
+  }
+}
