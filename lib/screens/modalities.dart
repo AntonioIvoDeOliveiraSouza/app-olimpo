bd67977990fbbcf0731106f1ec08adbe30692749
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-09-29 16:11:34 -0300

    fix: inserting gridItem navigator

--- lib/screens/modalities.dart
+++ lib/screens/modalities.dart
@@ -2,6 +2,7 @@ import 'package:olimpo/widgets/modality_grid_item.dart';
 import 'package:flutter/material.dart';
 import 'package:olimpo/data/olympic_data.dart';
 import 'package:olimpo/widgets/nav_bar.dart';
+import 'package:olimpo/screens/sport.dart';
 
 class ModalitiesScreen extends StatelessWidget{
   const ModalitiesScreen ({super.key});
@@ -23,7 +24,14 @@ class ModalitiesScreen extends StatelessWidget{
             mainAxisSpacing: 20),
         children: [
           for (final modality in listModality)
-            ModalityGridItem(modality: modality)
+            GridItem(
+              item: modality,
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (_) => SportScreen(sport: modality)),
+                );
+              }
+            )
         ],),
       bottomNavigationBar: NavBar(parentContext: context),
     );
