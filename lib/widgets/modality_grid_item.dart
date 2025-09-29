514d5db2ab221f5712860ce8c573ad29be73620e
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-09-29 16:05:09 -0300

    update: allowing widget grid modality and athlete data

--- lib/widgets/modality_grid_item.dart
+++ lib/widgets/modality_grid_item.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:olimpo/models/modality.dart';
-import 'package:olimpo/screens/sport.dart';
+import 'package:olimpo/models/basic_model.dart';
 
-class ModalityGridItem extends StatelessWidget {
-  const ModalityGridItem({
+class GridItem extends StatelessWidget {
+  const GridItem({
     super.key,
-    required this.modality,
+    required this.item,
+    required this.onTap,
   });
 
-  final Modality modality;
+  final GridItemData item;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +19,7 @@ class ModalityGridItem extends StatelessWidget {
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16),
             image: DecorationImage(
-              image: NetworkImage(modality.imageUrl),
+              image: NetworkImage(item.imageUrl),
               fit: BoxFit.cover,
               colorFilter: ColorFilter.mode(
                 Colors.black.withValues(alpha: 0.3),
@@ -30,34 +31,26 @@ class ModalityGridItem extends StatelessWidget {
         Material(
           color: Colors.transparent,
           child: InkWell(
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) {
-                  return SportScreen(
-                    sport: modality,
-                  );
-                }),
-              );
-            },
+            onTap: onTap,
             splashColor: Theme.of(context).primaryColor.withValues(alpha: 0.6),
-            highlightColor: Theme.of(context).primaryColor.withValues(alpha:0.3 ),
+            highlightColor: Theme.of(context).primaryColor.withValues(alpha: 0.3),
             borderRadius: BorderRadius.circular(16),
             child: Container(
               padding: const EdgeInsets.all(12),
               child: Center(
                 child: Text(
-                  modality.title,
+                  item.title,
                   style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    shadows: [
-                      Shadow(
-                        blurRadius: 4.0,
-                        color: Colors.black.withValues(alpha: 0.5),
-                        offset: const Offset(2.0, 2.0),
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 4.0,
+                            color: Colors.black.withValues(alpha: 0.5),
+                            offset: const Offset(2.0, 2.0),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
                   textAlign: TextAlign.center,
                 ),
               ),
