3b3ec44ae1f80f2786384ad7e8240e35d5319fcb
julijubamv <julijuba.vieira@gmail.com>
2025-10-03 15:08:24 -0300

    Update menu_screen.dart

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -15,7 +15,45 @@ class MenuScreen extends StatelessWidget {
         foregroundColor: Colors.white,
         backgroundColor: Colors.blue,
       ),
-      drawer: Drawer(), //TODO: Elaborar Sidedrawer
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child:  
+                Text('Olimpo', style: TextStyle(color: Colors.white, fontSize: 24)),
+                Text('Membros:', style: TextStyle(color: Colors.white, fontSize: 16)),
+                Text('Antonio Ivo, Giovanna, Hyann Gabriel e Juliana', style: TextStyle(color: Colors.white, fontSize: 16)),
+                Text('Aplicação: Olimpo: informações sobre Olimpíadas', style: TextStyle(color: Colors.white, fontSize: 16)),
+            ),
+            ListTile(
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Modalidades'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Atletas'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Sobre'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ), //TODO: Elaborar Sidedrawer;
       body: Center(
         child: Image.asset(
           'lib/data/images/logo.png',
