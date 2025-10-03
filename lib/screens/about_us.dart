d8bdad777849138c7b300dfb8a2743ac879aecc7
julijubamv <julijuba.vieira@gmail.com>
2025-10-03 15:44:30 -0300

    Create about_us.dart

---
+++ lib/screens/about_us.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AboutUs extends StatelessWidget {
+  const AboutUs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'Olimpo';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+            title: const Text(appTitle)
+        ),
+        body: const Column(
+            children: const [
+                Center(
+                    child: Text('Aplicação: Olimpo', style: TextStyle(color: Colors.white, fontSize: 24)),
+                    child: Text('Membros do grupo: Antonio Ivo, Giovanna, Hyann Gabriel e Juliana', style: TextStyle(color: Colors.white, fontSize: 16)),
+                ),
+            ],
+        ),
+      ),
+    );
+  }
+}
