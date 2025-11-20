7bb19db0e1ee03268996d565d8c448b147a9d595
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-11-20 15:17:37 -0300

    update: changing brightness

--- lib/config/preference_theme.dart
+++ lib/config/preference_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class PreferenceTheme {
-  static ValueNotifier<Brightness> tema = ValueNotifier(Brightness.dark);
+  static ValueNotifier<Brightness> tema = ValueNotifier(Brightness.light);
 
   static setTema(){
     tema.value = WidgetsBinding.instance.platformDispatcher.platformBrightness;
