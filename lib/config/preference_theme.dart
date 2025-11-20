7cdacf0652eb89d739203f1f0e0816a21186cd36
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-11-20 15:51:02 -0300

    update: setting toggle theme method

--- lib/config/preference_theme.dart
+++ lib/config/preference_theme.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 class PreferenceTheme {
   static ValueNotifier<Brightness> tema = ValueNotifier(Brightness.light);
 
-  static setTema(){
+  static void setTema(){
     tema.value = WidgetsBinding.instance.platformDispatcher.platformBrightness;
+  }
+
+  static void toggleTema(){
+    tema.value = tema.value == Brightness.dark ? Brightness.light : Brightness.dark;
   }
 }
