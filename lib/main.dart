06ca95a49ad2cc021f264284b2307088a3071b4e
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-11-20 15:18:07 -0300

    changing stateless to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:olimpo/screens/menu_screen.dart';
+import 'package:olimpo/config/preference_theme.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    PreferenceTheme.setTema(); 
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    PreferenceTheme.setTema();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: MenuScreen()
+    return ValueListenableBuilder(
+      valueListenable: PreferenceTheme.tema,
+      builder: (BuildContext context, Brightness brightness, Widget? _) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.deepPurple,
+            brightness: brightness, 
+          ),
+          home: const MenuScreen(),
+        );
+      },
     );
   }
 }
