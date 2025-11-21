f22b057fa8c591800ca610d6fc0b2d4314583ba3
Antonio Ivo De Oliveira Souza <2005antonioivo@gmail.com>
2025-11-21 10:03:01 -0300

    feature: new method change color

--- lib/config/preference_theme.dart
+++ lib/config/preference_theme.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 
 class PreferenceTheme {
   static ValueNotifier<Brightness> tema = ValueNotifier(Brightness.light);
+  static ValueNotifier<MaterialColor> color = ValueNotifier(Colors.green);
 
   static void setTema(){
     tema.value = WidgetsBinding.instance.platformDispatcher.platformBrightness;
+  }
+
+  static void changeColor(MaterialColor newColor){
+    color.value = newColor;
   }
 
   static void toggleTema(){
